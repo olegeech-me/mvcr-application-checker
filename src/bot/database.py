bff21ef44a811ca499970d023af0d1ae1b039cfc
Oleg Basov <oleg.basov@thalesgroup.com>
2023-09-27 11:26:04 +0200

    Fix DB race condition
    Closes #5

--- src/bot/database.py
+++ src/bot/database.py
@@ -17,17 +17,19 @@ class Database:
         self.host = host
         self.port = port
         self.loop = loop
-        self.conn = None
+        self.pool = None
 
     async def connect(self, max_retries=MAX_RETRIES, delay=RETRY_DELAY):
         for attempt in range(1, max_retries + 1):
             try:
-                self.conn = await asyncpg.connect(
+                self.pool = await asyncpg.create_pool(
                     database=self.dbname,
                     user=self.user,
                     password=self.password,
                     host=self.host,
                     port=self.port,
+                    min_size=5,
+                    max_size=20,
                 )
                 logger.info("Connected to the DB")
                 break
@@ -69,88 +71,96 @@ class Database:
             first_name,
             last_name,
         )
-        try:
-            await self.conn.execute(query, *params)
-            return True
-        except asyncpg.UniqueViolationError:
-            logger.error(f"Attempt to insert duplicate chat ID {chat_id} and application number {application_number}")
-            return False
-        except Exception as e:
-            logger.error(
-                f"Error while inserting into DB for chat ID: {chat_id} for application number {application_number}. Error: {e}"
-            )
-            return False
+        async with self.pool.acquire() as conn:
+            try:
+                await conn.execute(query, *params)
+                return True
+            except asyncpg.UniqueViolationError:
+                logger.error(f"Attempt to insert duplicate chat ID {chat_id} and application number {application_number}")
+                return False
+            except Exception as e:
+                logger.error(
+                    f"Error while inserting into DB for chat ID: {chat_id} "
+                    f"for application number {application_number}. Error: {e}"
+                )
+                return False
 
     async def update_db_status(self, chat_id, current_status, is_resolved):
         logger.info(f"Updating chatID {chat_id} current status in DB")
         query = "UPDATE Applications SET current_status = $1, last_updated = CURRENT_TIMESTAMP, is_resolved=$2 WHERE chat_id = $3"
         params = (current_status, is_resolved, chat_id)
-        try:
-            await self.conn.execute(query, *params)
-            return True
-        except Exception as e:
-            logger.error(f"Error while updating DB for chat ID: {chat_id}. Error: {e}")
-            return False
+        async with self.pool.acquire() as conn:
+            try:
+                await conn.execute(query, *params)
+                return True
+            except Exception as e:
+                logger.error(f"Error while updating DB for chat ID: {chat_id}. Error: {e}")
+                return False
 
     async def remove_from_db(self, chat_id):
         logger.info(f"Removing chatID {chat_id} from DB")
         query = "DELETE FROM Applications WHERE chat_id = $1"
-        try:
-            await self.conn.execute(query, chat_id)
-            return True
-        except Exception as e:
-            logger.error(f"Error while updating DB for chat ID: {chat_id}. Error: {e}")
-            return False
+        async with self.pool.acquire() as conn:
+            try:
+                await conn.execute(query, chat_id)
+                return True
+            except Exception as e:
+                logger.error(f"Error while updating DB for chat ID: {chat_id}. Error: {e}")
+                return False
 
     async def get_user_data_from_db(self, chat_id):
         """Fetch all user data for a given chat_id."""
         query = """
             SELECT * FROM Applications WHERE chat_id = $1;
         """
-        try:
-            row = await self.conn.fetchrow(query, chat_id)
-            if row is None:
-                logger.info(f"No data found for chat_id {chat_id}")
+        async with self.pool.acquire() as conn:
+            try:
+                row = await conn.fetchrow(query, chat_id)
+                if row is None:
+                    logger.info(f"No data found for chat_id {chat_id}")
+                    return None
+                return dict(row)  # Convert the record to a dictionary
+            except Exception as e:
+                logger.error(f"Error while fetching user data for chat ID: {chat_id}. Error: {e}")
                 return None
-            return dict(row)  # Convert the record to a dictionary
-        except Exception as e:
-            logger.error(f"Error while fetching user data for chat ID: {chat_id}. Error: {e}")
-            return None
 
     async def get_application_status(self, chat_id):
         query = "SELECT current_status FROM Applications WHERE chat_id = $1;"
-        try:
-            result = await self.conn.fetchval(query, chat_id)
-            return result
-        except Exception as e:
-            logger.error(f"Error while fetching application status for chat ID: {chat_id}. Error: {e}")
-            return None
+        async with self.pool.acquire() as conn:
+            try:
+                result = await conn.fetchval(query, chat_id)
+                return result
+            except Exception as e:
+                logger.error(f"Error while fetching application status for chat ID: {chat_id}. Error: {e}")
+                return None
 
     async def get_application_status_timestamp(self, chat_id):
         query = "SELECT current_status, last_updated FROM Applications WHERE chat_id = $1;"
-        try:
-            result = await self.conn.fetchrow(query, chat_id)
-            if result is not None:
-                current_status = result["current_status"]
-                last_updated_utc = result["last_updated"].replace(tzinfo=pytz.utc)
-                last_updated_prague = last_updated_utc.astimezone(pytz.timezone("Europe/Prague"))
-                timestamp = last_updated_prague.strftime("%H:%M:%S %d-%m-%Y")
+        async with self.pool.acquire() as conn:
+            try:
+                result = await conn.fetchrow(query, chat_id)
+                if result is not None:
+                    current_status = result["current_status"]
+                    last_updated_utc = result["last_updated"].replace(tzinfo=pytz.utc)
+                    last_updated_prague = last_updated_utc.astimezone(pytz.timezone("Europe/Prague"))
+                    timestamp = last_updated_prague.strftime("%H:%M:%S %d-%m-%Y")
 
-                return f"Current Status: {current_status}\nLast Updated: <b>{timestamp}</b>"
-            else:
-                return "No data found."
-        except Exception as e:
-            logger.error(f"Error while fetching status from DB for chat ID: {chat_id}. Error: {e}")
-            return "Current status is empty. Please wait some time and try again."
+                    return f"Current Status: {current_status}\nLast Updated: <b>{timestamp}</b>"
+                else:
+                    return "No data found."
+            except Exception as e:
+                logger.error(f"Error while fetching status from DB for chat ID: {chat_id}. Error: {e}")
+                return "Current status is empty. Please wait some time and try again."
 
     async def check_subscription_in_db(self, chat_id):
         query = "SELECT EXISTS(SELECT chat_id FROM Applications WHERE chat_id=$1)"
-        try:
-            result = await self.conn.fetchval(query, chat_id)
-            return result
-        except Exception as e:
-            logger.error(f"Error while checking chat_id {chat_id} subscription. Error: {e}")
-            return False
+        async with self.pool.acquire() as conn:
+            try:
+                result = await conn.fetchval(query, chat_id)
+                return result
+            except Exception as e:
+                logger.error(f"Error while checking chat_id {chat_id} subscription. Error: {e}")
+                return False
 
     async def get_applications_needing_update(self, refresh_period):
         # Convert the timedelta refresh period to seconds for the SQL interval.
@@ -164,33 +174,36 @@ class Database:
             AND is_resolved = FALSE
         """
 
-        try:
-            return await self.conn.fetch(query, seconds)
-        except Exception as e:
-            logger.error(f"Error while fetching applications needing update. Error: {e}")
-            return []
+        async with self.pool.acquire() as conn:
+            try:
+                return await conn.fetch(query, seconds)
+            except Exception as e:
+                logger.error(f"Error while fetching applications needing update. Error: {e}")
+                return []
 
     async def update_timestamp(self, chat_id):
         logger.info(f"Updating last_updated timestamp for chatID {chat_id} in DB")
         query = "UPDATE Applications SET last_updated = CURRENT_TIMESTAMP WHERE chat_id = $1"
-        try:
-            await self.conn.execute(query, chat_id)
-        except Exception as e:
-            logger.error(f"Error while updating timestamp for chat ID: {chat_id}. Error: {e}")
+        async with self.pool.acquire() as conn:
+            try:
+                await conn.execute(query, chat_id)
+            except Exception as e:
+                logger.error(f"Error while updating timestamp for chat ID: {chat_id}. Error: {e}")
 
     async def get_subscribed_user_count(self):
         """Return the count of unique users subscribed"""
         query = "SELECT COUNT(DISTINCT chat_id) FROM Applications;"
-        try:
-            count = await self.conn.fetchval(query)
-            return count
-        except Exception as e:
-            logger.error(f"Error while fetching subscribed user count. Error: {e}")
-            return None
+        async with self.pool.acquire() as conn:
+            try:
+                count = await conn.fetchval(query)
+                return count
+            except Exception as e:
+                logger.error(f"Error while fetching subscribed user count. Error: {e}")
+                return None
 
     async def close(self):
         logger.info("Shutting down DB connection")
         try:
-            await self.conn.close()
+            await self.pool.close()
         except Exception as e:
             logger.error(f"Error while shutting down DB connection. Error: {e}")
