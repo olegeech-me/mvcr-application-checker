e97247726c636f531547d4704c5e926e4da7c8c6
Oleg Basov <oleg.basov@thalesgroup.com>
2023-10-05 01:27:39 +0200

    lower logging level

--- src/bot/handlers.py
+++ src/bot/handlers.py
@@ -35,8 +35,7 @@ async def _set_menu_commands(update: Update, context: ContextTypes.DEFAULT_TYPE,
     logger.info(f"Setting menu commands for {user_info(update)}, language: {lang}")
     commands = [BotCommand(cmd, commands_description[lang][cmd]) for cmd in COMMANDS_LIST]
     if _is_admin(update.effective_chat.id):
-        # tmp to debug issues:
-        logger.info(f"Adding admin menu commands for chat_id {update.effective_chat.id}, ADMIN_CHAT_ID={ADMIN_CHAT_ID}")
+        logger.debug(f"Adding admin menu commands for chat_id {update.effective_chat.id}, ADMIN_CHAT_ID={ADMIN_CHAT_ID}")
         commands.extend([BotCommand(cmd, commands_description[lang][cmd]) for cmd in ADMIN_COMMANDS])
     await context.bot.set_my_commands(commands, BotCommandScopeChat(update.effective_chat.id))
 
