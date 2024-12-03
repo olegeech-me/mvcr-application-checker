043c35556a345eea1665911781a89fc4e8df99a1
Oleg Basov <olegeech@sytkovo.su>
2024-12-03 03:16:50 +0100

    Set env variables correctly

--- src/bot/tracing.py
+++ src/bot/tracing.py
@@ -6,9 +6,9 @@ from opentelemetry.exporter.zipkin.json import ZipkinExporter
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from bot.loader import TRACING_DISABLED, ZIPKIN_ENDPOINT, ZIPKIN_TIMEOUT
 
-os.environ("OTEL_SDK_DISABLED", TRACING_DISABLED)
-os.environ("OTEL_EXPORTER_ZIPKIN_ENDPOINT", ZIPKIN_ENDPOINT)
-os.environ("OTEL_EXPORTER_ZIPKIN_TIMEOUT", ZIPKIN_TIMEOUT)
+os.environ["OTEL_SDK_DISABLED"] = TRACING_DISABLED
+os.environ["OTEL_EXPORTER_ZIPKIN_ENDPOINT"] = ZIPKIN_ENDPOINT
+os.environ["OTEL_EXPORTER_ZIPKIN_TIMEOUT"] = ZIPKIN_TIMEOUT
 
 
 resource = Resource(attributes={"service.name": "mvcr-telegram-bot"})
